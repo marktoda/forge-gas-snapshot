f9c916975621f41893f5b609c27c9c69387eb9c8
Mark Toda <toda.mark@gmail.com>
2022-08-05 17:35:44 -0700

    feat: test accuracy

--- src/test/SimpleOperations.sol
+++ src/test/SimpleOperations.sol
@@ -3,25 +3,20 @@ pragma solidity ^0.8.15;
 
 contract SimpleOperations {
     uint256 internal test;
-    string internal prefix;
 
-    constructor(string memory _prefix) {
-        prefix = _prefix;
-    }
-
-    function add() external {
+    function add() public pure {
         uint256 x = 1;
         x++;
     }
 
-    function manyAdd() external {
+    function manyAdd() public pure {
         uint256 x;
         for (uint256 i = 0; i < 100; i++) {
             x = i + 1;
         }
     }
 
-    function manySstore() external {
+    function manySstore() public {
         for (uint256 i = 0; i < 100; i++) {
             test = i + 2;
         }
