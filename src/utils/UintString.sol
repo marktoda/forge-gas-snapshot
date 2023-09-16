7d25970bf2a2aa52b495f97e370ef5aab6a505fc
Aryan Malik <88618913+Aryan9592@users.noreply.github.com>
2023-09-16 21:47:07 +0530

    Update UintString.sol: Enhanced loop

--- src/utils/UintString.sol
+++ src/utils/UintString.sol
@@ -7,8 +7,8 @@ library UintString {
     /// @notice converts the given string s into a uint256
     function stringToUint(string memory s) internal pure returns (uint256 result) {
         bytes memory b = bytes(s);
-        uint256 oldResult = 0;
-        for (uint256 i = 0; i < b.length; i++) {
+        uint256 oldResult;
+        for (uint256 i; i < b.length) {
             // c = b[i] was not needed
             if (uint8(b[i]) >= 48 && uint8(b[i]) <= 57) {
                 // store old value so we can check for overflows
@@ -20,6 +20,9 @@ library UintString {
                 }
             } else {
                 revert InvalidStringNumber(s);
+            }
+            unchecked {
+                ++i;
             }
         }
     }
