28edc75758165b43f2a2795a85cd9931b2bc6cc7
Mark Toda <toda.mark@gmail.com>
2024-09-09 11:16:28 -0400

    feat: remove console2

--- src/GasSnapshot.sol
+++ src/GasSnapshot.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
-import "forge-std/src/console2.sol";
 import {Script} from "forge-std/src/Script.sol";
 import {UintString} from "./utils/UintString.sol";
 
